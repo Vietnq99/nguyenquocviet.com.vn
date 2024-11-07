ad5437b69306442bd43d87fa5b18bc6c1ee17f41
Vietnq99 <125450441+Vietnq99@users.noreply.github.com>
2024-11-07 22:49:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
 function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-
-  // Đóng menu khi nhấp bên ngoài menu
-  document.addEventListener("click", function (event) {
-    if (!menu.contains(event.target) && !icon.contains(event.target)) {
-      menu.classList.remove("open");
-      icon.classList.remove("open");
-    }
-  }, { once: true });
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
 }
-
-// Đóng menu khi cuộn trang
-window.addEventListener("scroll", () => {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-
-  if (menu.classList.contains("open")) {
-    menu.classList.remove("open");
-    icon.classList.remove("open");
-  }
-});
